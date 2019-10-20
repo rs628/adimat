9379faf4a373e306f3780a29ee36302492958ba3
renu <rs628@snu.edu.in>
2019-10-20 16:21:32 +0530

    one more example is done with forward mode

--- autodiffmyfunctionmaster.m
+++ autodiffmyfunctionmaster.m
@@ -2,7 +2,8 @@
 
 X=linspace(0,10,11);
 x=X;
-Jac1 = admDiffFor(@myfunction, 1, x); Jac = admDiffRev(@myfunction, 1, x);
+Jacmyfunction = admDiffFor(@myfunction, 1, x); 
+%Jac = admDiffRev(@myfunction, 1, x);
 z=2.*x+2 ;
 %jac gives J*S
 % Jac=jacobian matrix
@@ -13,7 +14,8 @@ plot(x,y,'r')
 hold on
 plot(x,z,'b')
 hold on
-plot(x,Jac1,'g')
-plot(x,Jac1,'m')
+plot(x,Jacmyfunction,'o')
+% plot(x,Jac1,'m')
+
 
 
